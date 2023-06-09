356ecf2cca32a69d9dc518739e047b0440dc4030
kanngji <alleneffort1994@gmail.com>
2023-06-09 23:24:04 +0900

    first commit

---
+++ trading_django/config/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+# Create your views here.
+class Main(APIView):
+    def get(self,request):
+        return render(request, 'index.html')
