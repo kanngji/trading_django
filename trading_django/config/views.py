aa62b2dd4b1beff1f8a38558d6e1e12b92f8acb8
kanngji <alleneffort1994@gmail.com>
2023-06-10 11:48:41 +0900

    스크래핑 해서 차트 넣는거 말고 캐러샐로 변경 TODO: main내용추가, 회원가입, 로그인하기

--- trading_django/config/views.py
+++ trading_django/config/views.py
@@ -9,4 +9,8 @@ class Main(APIView):
     def get(self,request):
         # 로고 주소
         logo_url = "static/img/logo.PNG"
-        return render(request, 'index.html',{'logo_url':logo_url})
+        crs1 = "static/img/finbiz.PNG"
+        crs2 = "static/img/hangyoung.PNG"
+        crs3 = "static/img/tradingview.PNG"
+        context = {'logo_url':logo_url,'crs1':crs1,'crs2':crs2,'crs3':crs3}
+        return render(request, 'index.html',context)
